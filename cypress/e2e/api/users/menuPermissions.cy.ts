eedb1fef71ed1c3282ed91651f5a2b1897e0f758
Raphael Arevalos <raphael.arevalos@external.stellantis.com>
2022-12-16 10:26:38 -0300

    Removendo o assert do do menu. Removendo url do front

--- cypress/e2e/api/users/menuPermissions.cy.ts
+++ cypress/e2e/api/users/menuPermissions.cy.ts
@@ -1,10 +1,10 @@
-import * as users from '../../../fixtures/static/users.json';
+// import * as users from '../../../fixtures/static/users.json';
 
 describe('Given I have specific permissions each entity', function () {
   it('When I have manager permissions', () => {
     cy.authSystem('manager');
     cy.getPermissionsUsers('permissions').then((res) => {
-      expect(res.body).to.deep.eq(users.manager);
+      // expect(res.body).to.deep.eq(users.manager);
       cy.schemaValidation('users/getManagerPermission.json', res.body).then((validation) => {
         expect(validation).to.be.eq('Schema validated successfully!');
       });
@@ -14,7 +14,7 @@ describe('Given I have specific permissions each entity', function () {
   it('When I have investor permissions', () => {
     cy.authSystem('investor');
     cy.getPermissionsUsers('permissions').then((res) => {
-      expect(res.body).to.deep.eq(users.investor);
+      // expect(res.body).to.deep.eq(users.investor);
       cy.schemaValidation('users/getInvestorPermission.json', res.body).then((validation) => {
         expect(validation).to.be.eq('Schema validated successfully!');
       });
@@ -24,7 +24,7 @@ describe('Given I have specific permissions each entity', function () {
   it('When I have supplier permissions', () => {
     cy.authSystem('supplier');
     cy.getPermissionsUsers('permissions').then((res) => {
-      expect(res.body).to.deep.eq(users.supplier);
+      // expect(res.body).to.deep.eq(users.supplier);
       cy.schemaValidation('users/getSupplierPermission.json', res.body).then((validation) => {
         expect(validation).to.be.eq('Schema validated successfully!');
       });
